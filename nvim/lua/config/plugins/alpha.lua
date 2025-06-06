5e94760855c9f629043e4c95c76ad1c555342a60
shorya <shoryajainstedwards@gmail.com>
2025-06-06 13:19:14 +0530

    Minor updates

--- nvim/lua/config/plugins/alpha.lua
+++ nvim/lua/config/plugins/alpha.lua
@@ -26,7 +26,6 @@ return {
 
     -- Set menu
     dashboard.section.buttons.val = {
-      -- dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
       dashboard.button("<leader> ee", "  > Toggle file tree", "<cmd>NvimTreeToggle<CR>"),
       dashboard.button("<leader> ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
       dashboard.button("<leader> fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
