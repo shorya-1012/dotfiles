41ab14384c9ac461d39129eb10e4373825063d66
Shorya Jain <shoryajainstedwards@gmail.com>
2025-07-31 12:09:44 +0530

    Update rofi wallpaper selector

--- rofi/wallpaper/launcher.sh
+++ rofi/wallpaper/launcher.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
 WALLPAPER_DIR="$HOME/wallpapers"
+wallpapers=$(ls "$WALLPAPER_DIR")
 
-wallpaper=$(ls "$WALLPAPER_DIR" | rofi -dmenu -theme ~/.dotfiles/rofi/wallpaper/config.rasi )
+# Optional: Get current wallpaper name from nitrogen or swww
+current_wallpaper_name=$(basename "$(find ~/.config/nitrogen/bg-saved.cfg -type f -exec grep -m1 'file=' {} \; | cut -d'=' -f2 2>/dev/null)")
 
-if [ -z "$wallpaper" ]; then
-  exit
+rofi_list=""
+while IFS= read -r a; do
+    filename=$(basename "$a")
+    if [[ "$filename" == "$current_wallpaper_name" ]]; then
+        rofi_list+="${filename} (current)\0icon\x1f$WALLPAPER_DIR/$filename\n"
+    else
+        rofi_list+="${filename}\0icon\x1f$WALLPAPER_DIR/$filename\n"
+    fi
+done <<<"$wallpapers"
+
+selected_wallpaper=$(echo -e "$rofi_list" | rofi -dmenu -p "Select Wallpaper:" -theme "$HOME/.config/rofi/wallpaper/config.rasi" -markup-rows)
+
+if [ -z "$selected_wallpaper" ]; then
+  exit 0
 fi
+
+# Remove "(current)" if it's there
+selected_wallpaper=$(echo "$selected_wallpaper" | sed 's/ (current)//g')
 
 active_win=$(xdotool getactivewindow)
 
@@ -14,7 +31,8 @@ active_win=$(xdotool getactivewindow)
 xprop -id "$active_win" -f _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY 0x00000000
 sleep 0.2
 
-nitrogen --set-zoom-fill "$WALLPAPER_DIR/$wallpaper" --save
+nitrogen --set-zoom-fill "$WALLPAPER_DIR/$selected_wallpaper" --save
 
-# picom-trans -w $(xdotool getactivewindow) --set-opacity 1.0
+# Fade back in
 xprop -id "$active_win" -f _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY 0xffffffff
+
