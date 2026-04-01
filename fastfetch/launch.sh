0cf2e28bfab8ce39414a81685d6ddfe0b7a367e0
Shorya Jain <shoryajainstedwards@gmail.com>
2026-04-01 22:47:40 +0530

    Change: fastfetch, kitty theme

--- fastfetch/launch.sh
+++ fastfetch/launch.sh
@@ -1,5 +1,1 @@
-# LOGO_DIR="$HOME/.config/fastfetch/images/books"
-#
-# RANDOM_LOGO=$(find "$LOGO_DIR" -type f | shuf -n 1)
-#
-fastfetch --logo "$HOME/.dotfiles/fastfetch/images/vivian.png" --logo-type kitty
+fastfetch --logo "$HOME/.dotfiles/fastfetch/images/aigis.png" --logo-type kitty
